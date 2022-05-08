ae4b87ccffb1b00f9958e4b9419286b60e0dd511
Xingkun Zhang <1475795322@qq.com>
2022-05-08 13:43:33 +0800

    feat: make index normal show in mobile

--- src/switchAudio.js
+++ src/switchAudio.js
@@ -1,4 +1,3 @@
-// const jsmediatags = require('jsmediatags/dist/jsmediatags.min')
 let index = 0;
 let nextButtonClass;
 let preButtonClass;
@@ -16,21 +15,6 @@ const setMusicCover = (cover) => {
         document.documentElement.style.setProperty('--background-image', `url('https://avatars.githubusercontent.com/u/42455616?v=4')`)
     }
 };
-// let myUrl;
-// const getCoverFromMusic = (url) => {
-//     // getCoverFromMusic(window.location.protocol+'//'+window.location.host+'/'+mySrc)
-//
-//     jsmediatags.read(url, {
-//         onSuccess: function (tag) {
-//             myUrl = "data:image/png;base64," + btoa(String.fromCharCode.apply(null, new Uint8Array(tag.tags.picture.data)));
-//             setMusicCover(myUrl)
-//         },
-//         onError: function (error) {
-//             console.log(error);
-//         }
-//     });
-// }
-
 
 const naturalSwitch = (audio, musics) => {
     audio.src = musics[0].url
